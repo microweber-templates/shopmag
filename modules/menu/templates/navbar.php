6f3cee407509803cb6838d4351f010262e531988
Nikolay <rachkuw@gmail.com>
2021-07-07 11:08:51 +0300

    update

--- modules/menu/templates/navbar.php
+++ modules/menu/templates/navbar.php
@@ -30,10 +30,10 @@ $menu_filter['li_class'] = 'shopmag-header-li';
 $menu_filter['a_class'] = 'shopmag-header-a';
 
 $menu_filter['li_submenu_class'] = 'dropdown';
-$menu_filter['li_submenu_a_class'] = 'dropdown-toggle';
+$menu_filter['li_submenu_a_class'] = 'dropdown-has-child';
 
-$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {a_class}"  href="{url}">{title}</a>';
-$menu_filter['li_submenu_a_link'] = '<a itemprop="url" data-item-id="{id}" href="{url}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {li_submenu_a_class}" aria-haspopup="true" aria-expanded="false"><span class="name">{title}</span> <span class="caret"></span></a>';
+$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {a_class}"  href="{url}"><span>{title}</span></a>';
+$menu_filter['li_submenu_a_link'] = '<a itemprop="url"  data-item-id="{id}" href="{url}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {li_submenu_a_class}" aria-haspopup="true" aria-expanded="false"><span class="name">{title}</span> <span class="caret"></span></a>';
 
 $mt = menu_tree($menu_filter);
 
@@ -43,3 +43,4 @@ if ($mt != false) {
     print lnotif("There are no items in the menu <b>" . $params['menu-name'] . '</b>');
 }
 ?>
+
