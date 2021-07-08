0362dcc55c5f2c8e828ba1d23fd7f11a2943123a
Nikolay <rachkuw@gmail.com>
2021-07-08 16:21:05 +0300

    update

--- modules/posts/templates/skin-1.php
+++ modules/posts/templates/skin-1.php
@@ -13,30 +13,32 @@ description: Posts Slider
 
 <?php include('slick_options.php'); ?>
 
-<div class="row blog-posts slickslider">
-    <?php if (!empty($data)): ?>
-        <?php foreach ($data as $item): ?>
-            <div class="row m-0 post">
-                <div class="col-12 d-flex flex-column h-100" itemscope itemtype="<?php print $schema_org_item_type_tag ?>">
-                    <div class="description">
-                        <?php if (!isset($show_fields) or $show_fields == false or in_array('title', $show_fields)): ?>
-                            <h3 class=" m-b-20"><?php print $item['title'] ?></h3>
-                        <?php endif; ?>
+<div class="container-fluid">
+    <div class="row blog-posts slickslider">
+        <?php if (!empty($data)): ?>
+            <?php foreach ($data as $item): ?>
+                <div class="row m-0 post">
+                    <div class="col-12 d-flex flex-column h-100" itemscope itemtype="<?php print $schema_org_item_type_tag ?>">
+                        <div class="description">
+                            <?php if (!isset($show_fields) or $show_fields == false or in_array('title', $show_fields)): ?>
+                                <h3 class=" m-b-20"><?php print $item['title'] ?></h3>
+                            <?php endif; ?>
 
-                        <p class="date m-b-10"><?php print $item['created_at'] ?></p>
+                            <p class="date m-b-10"><?php print $item['created_at'] ?></p>
 
-                        <?php if (!isset($show_fields) or $show_fields == false or in_array('description', $show_fields)): ?>
-                            <p><?php print $item['description'] ?></p>
-                        <?php endif; ?>
-                    </div>
+                            <?php if (!isset($show_fields) or $show_fields == false or in_array('description', $show_fields)): ?>
+                                <p><?php print $item['description'] ?></p>
+                            <?php endif; ?>
+                        </div>
 
-                    <div class="m-t-auto">
-                        <a href="<?php print $item['link'] ?>" class="button-8"><span>Read more</span></a>
+                        <div class="m-t-auto">
+                            <a href="<?php print $item['link'] ?>" class="button-8"><span>Read more</span></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
 </div>
 
 <?php if (isset($pages_count) and $pages_count > 1 and isset($paging_param)): ?>
