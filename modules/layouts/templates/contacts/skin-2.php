0f85804fee7e7cfac153e2af612ca53a51887796
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-07-09 12:09:31 +0300

    update

--- modules/layouts/templates/contacts/skin-2.php
+++ modules/layouts/templates/contacts/skin-2.php
@@ -45,7 +45,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
                             <div class="col-12 cloneable">
                                 <div class="shadow-md box">
                                     <div class="icon-holder">
-                                        <i class="mdi mdi-house safe-element"></i>
+                                        <i class="mdi mdi-address safe-element"></i>
                                     </div>
                                     <div class="text-holder allow-drop">
                                         <p><strong>Address</strong></p>
@@ -69,7 +69,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
                             <div class="col-12 cloneable">
                                 <div class="shadow-md box">
                                     <div class="icon-holder">
-                                        <i class="mdi mdi-email safe-element"></i> x
+                                        <i class="mdi mdi-email safe-element"></i>
                                     </div>
                                     <div class="text-holder allow-drop">
                                         <p><strong>Other ways to contact us</strong></p>
