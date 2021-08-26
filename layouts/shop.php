92fb0d21c989c93c174eaae4168ed7018c766b5a
Nikolay <rachkuw@gmail.com>
2021-08-26 11:05:10 +0300

    update

--- layouts/shop.php
+++ layouts/shop.php
@@ -7,37 +7,24 @@ content_type: dynamic
 name: Shop
 is_shop: y
 description: Showcase shop items in a sylish grid arrangement.
-position: 2
+position: 4
 */
+
 ?>
 
 <?php include template_dir() . "header.php"; ?>
-
-<?php
-if (!isset($shop_sidebar)) {
-    $shop_sidebar = false;
-}
-?>
-
 
 <div class="edit" rel="content" field="content">
     <module type="layouts" template="skin-16"/>
 </div>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12 mx-auto">
-            <div class="row justify-content-between">
 
-                <module type="shop" />
 
-            </div>
-        </div>
-    </div>
-</div>
+<module type="shop" />
+
+
 <div class="edit" rel="content" field="shop-after-content">
     <p class="element"></p>
 </div>
-
 
 <?php include template_dir() . "footer.php"; ?>
