a731b0cd1b7e8a2e5bd13233f2da11a9aee78db8
Nikolay <rachkuw@gmail.com>
2021-11-04 18:27:05 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = array();
 $config['name'] = "Shopmag";
 $config['author'] = "Microweber CMS";
-$config['version'] = 1.4;
+$config['version'] = 2;
 $config['url'] = "https://microweber.org";
 $config['standalone_module_skins'] = true;
 $config['framework'] = "bootstrap5";
