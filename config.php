7b26f250e53e8d1cbdad82c26877f289f525f2c1
Peter Ivanov <peter@microweber.com>
2023-02-28 11:46:46 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = array();
 $config['name'] = "Shopmag";
 $config['author'] = "Microweber CMS";
-$config['version'] = '4.1';
+$config['version'] = '4.3';
 $config['url'] = "https://microweber.org";
 $config['standalone_module_skins'] = true;
 $config['framework'] = "bootstrap5";
